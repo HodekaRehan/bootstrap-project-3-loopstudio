4954ddfd126f68d3a14f9f684d8b50bb6c3aec8f
ReyHaan <hodekarehan@gmail.com>
2022-03-04 22:38:38 +0530

    header completed

---
+++ script.js
@@ -0,0 +1,19 @@
+const navbar = document.querySelector('.navbar')
+const hamburger = document.querySelector('.navbar-toggler');
+const navMenu = document.querySelector('.collapse');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+
+    if(hamburger.classList.contains('open')) {
+        hamburger.classList.remove('open');
+        navMenu.classList.remove('open');
+        navbar.classList.remove('open');
+
+    } else {
+        hamburger.classList.add('open');
+        navMenu.classList.add('open');
+        navbar.classList.add('open');
+    }
+}
